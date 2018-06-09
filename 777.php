774b1124f0b010e9294f2188dfeae2c4000a3dd4
phitthaya <37451527+phitthaya@users.noreply.github.com>
2018-06-10 01:33:46 +0700

    Update 777.php

--- 777.php
+++ 777.php
@@ -43,6 +43,18 @@
         $arrayPostData['messages'][0]['latitude'] = "13.7465354";
         $arrayPostData['messages'][0]['longitude'] = "100.532752";
         replyMsg($arrayHeader,$arrayPostData);
+        
+    else if($message == "1"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "template";
+        $arrayPostData['messages'][0]['altText'] = "this is a buttons template";
+        $arrayPostData['messages'][0]['template'] = "";
+        $arrayPostData['messages'][0]['type'] = "template";
+        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+        $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+        $arrayPostData['messages'][0]['latitude'] = "13.7465354";
+        $arrayPostData['messages'][0]['longitude'] = "100.532752";
+        replyMsg($arrayHeader,$arrayPostData);    
     }
     #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
     else if($message == "ลาก่อน"){
